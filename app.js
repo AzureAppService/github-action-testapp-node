2542b1a334b4a050644d937c82aa1f61e6a99e5d
Mitren Chinoy <nertim@users.noreply.github.com>
2020-09-16 17:36:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT, function () {
+  console.log('Example app listening on process.env.PORT');
 });

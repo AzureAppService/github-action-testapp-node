804ab3e410b878ab6593677cb9d52bb595aafca6
Hila Menashe <102023453+hila-m@users.noreply.github.com>
2022-03-27 22:13:53 +0300

    revert

--- app.js
+++ app.js
@@ -6,13 +6,3 @@ app.get('/', function (req, res) {
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on process.env.PORT');
 });
-
-
-import { ApplicationInsights } from '@microsoft/applicationinsights-web'
-
-const appInsights = new ApplicationInsights({ config: {
-  instrumentationKey: '71672876-2d97-45e9-bcbc-107d2f2a28ff';
-  /* ...Other Configuration Options... */
-} });
-appInsights.loadAppInsights();
-appInsights.trackPageView(); // Manually call trackPageView to establish the current user/session/pageview

72967b9184942698de88c2585c25a468e4a139bd
Hila Menashe <102023453+hila-m@users.noreply.github.com>
2022-03-29 10:40:23 +0300

    crashing the app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
+  res.send (4/0);
 });
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on process.env.PORT');
